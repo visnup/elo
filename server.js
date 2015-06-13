aeac8be2805d96d5ab9bef90ce73ea73c004db82
Visnu Pitiyanuvath <visnupx@gmail.com>
2015-06-13 03:01:34 -0700

    static cached

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ require('./models');
 // Middleware
 app.use(require('koa-conditional-get')());
 app.use(require('koa-etag')());
-app.use(require('koa-static')(path.resolve(__dirname, 'public'), {
-  maxage: 31536000  // 1y
+app.use(require('koa-static-cache')(path.join(__dirname, 'public'), {
+  maxAge: 365 * 24 * 60 * 60  // 1y
 }));
 app.use(require('koa-logger')());
 
